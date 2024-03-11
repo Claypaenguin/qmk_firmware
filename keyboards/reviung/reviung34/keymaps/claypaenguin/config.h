21fbfa93cf4ce17271d4a457aadeaac2bf757eb6
Claypaenguin <daenny783@gmail.com>
2024-03-11 12:11:17 +0100

    reset double tap

--- keyboards/reviung/reviung34/keymaps/claypaenguin/config.h
+++ keyboards/reviung/reviung34/keymaps/claypaenguin/config.h
@@ -36,3 +36,7 @@
 #define RGBLIGHT_EFFECT_RGB_TEST
 #define RGBLIGHT_EFFECT_ALTERNATING
 #define RGBLIGHT_EFFECT_TWINKLE
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED D5 // Specify an optional status LED by GPIO number which blinks when entering the bootloader
